1c60b93ddb8153560105df31bec3a09d899f5461
MasonCodyCox <masonfraser3@googlemail.com>
2020-12-08 00:46:11 +0000

    Impliments timeout function that hides the cards if not equal. Bug discovered that hides cards if you dont click another fast enough or even if they do match.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,31 @@
-console.log("hello");
+let clickedCard = null;
 
 function onCardClicked(e) {
     const target = e.currentTarget;
-    console.log('clicked', e.currentTarget);
+
+    if (target === clickedCard || target.className.includes('done')) {
+        return;
+    }
+
+
     target.className = target.className
         .replace('color-hidden', '')
         .trim();
+
+
+    if (!clickedCard) {
+        clickedCard = target;
+    }
+    else if (clickedCard) {
+        if (clickedCard.getAttribute('data-color') === target.getAttribute('data-color')) {
+
+            console.log('cards do match');
+        } else {
+            console.log('cards do not match');
+            setTimeout(() => {
+                clickedCard.className = clickedCard.className .replace('done', '').trim() + 'color-hidden';
+                target.className = target.className.replace('done', '').trim() + 'color-hidden';
+            }, 500);
+        }
+    }
 }
