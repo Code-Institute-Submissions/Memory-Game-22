84686a46dd1d3cba704dcbe4a97ac44668b9d711
MasonCodyCox <masonfraser3@googlemail.com>
2020-12-13 19:42:30 +0000

    creates new pages 'easy, medium and hard memory game levels. Copies code for original single level game onto all 3 pages so they are currently identical. removes the how to play page.'

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "mason", {
         "from_name": contactForm.name.value,
-        "from_email":contactForm.emailaddress.value,
-        "support_message":contactForm.supportdescription.value
+        "from_email": contactForm.emailaddress.value,
+        "support_message": contactForm.supportdescription.value
     })
-    .then(
-        function(response) {
-            console.log("SENT!", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-        return false;
+        .then(
+            function (response) {
+                console.log("SENT!", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            });
+    return false;
 }
